5aab421cab034f8644a0b0c71e36cf33c6bc89d6
mikekats <mkatsigiannis@gmail.com>
2020-02-05 17:43:43 +0200

    first commit

---
+++ routing1.py
@@ -0,0 +1,14 @@
+# n the number of nodes
+n = 4
+
+# l the number of links
+l = // n factorial  divided by  n-2 factorial ???
+
+nodes =  
+links =
+source =
+destination =
+directed_links =
+weighted_linkes = 
+
+# Shortest path (minimum cost path from one node to another)
